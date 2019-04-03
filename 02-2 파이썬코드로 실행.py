806b65995967b960f982b4c7232c184c8b1ce24f
jinbeom <jinbomang@gmail.com>
2019-04-03 10:39:30 +0900

    update

---
+++ 02-2 파이썬코드로 실행.py
@@ -0,0 +1,16 @@
+# -*-coding: utf-8 -*-
+
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+
+#Jupyter Notebook Test
+#주피터 처음 실행해보기
+#주피터의 기본기능 익혀보기
+print('Hello World')
+print('저는 이진범입니다')
+
+#그래프 그려보기
+plt.plot([1, 2, 3, 4])
+plt.ylabel('Number')
+plt.show()
